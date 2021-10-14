a4398c6a7ec9a9ee3d6348c4fa74707a1cdf2c68
Daan7777 <Detvanmontfoort@gmail.com>
2021-10-14 14:14:48 +0200

    tailwind 1.0

--- themes/OnzeTheme/assets/css/tailwind.config.js
+++ themes/OnzeTheme/assets/css/tailwind.config.js
@@ -1,8 +1,17 @@
+// Dit is tailwind.config.js iets met javascript waarmee je classes kan maken.
+// Bijvoorbeeld: bg-blue-500 (bg-blue betekend pak kleur blauw, en hoe hoger het getal, hoe donkerder de kleur).
+const colors = require('tailwindcss/colors')
 module.exports = {
-    theme: {
-      extend: {}
-    },
-    variants: {},
-    plugins: []
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    }
   }
-  
+}
